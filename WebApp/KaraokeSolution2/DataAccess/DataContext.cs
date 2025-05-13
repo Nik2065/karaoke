eb854fb59bbfb968e8b9f178fe154086c5c1e29b
Nik2065 <ivanov_nikolay@bk.ru>
2025-05-13 14:23:33 +0300

    доработки

---
+++ WebApp/KaraokeSolution2/DataAccess/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to mysql with connection string from app settings
+            var connectionString = Configuration.GetConnectionString("DatabaseConnectionStr");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebApp/KaraokeSolution2/KaraokeWebApp/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using DataAccess;
+using KaraokeWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KaraokeWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration conf)
+        {
+            _logger = logger;
+            db = new DataContext(conf);
+        }
+
+        private DataContext db;
+        
+
+        public IActionResult Index()
+        {
+            var one = db.Users.ToList();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        public IActionResult SignInAsClient()
+        {
+            return View();
+        }
+
+
+        public IActionResult SignInAsManager()
+        {
+            return View();
+        }
+
+        public IActionResult StartPage()
+        {
+            return View();
+        }
+
+        public IActionResult BookingPageForClient()
+        {
+            return View();
+        }
+
+        public IActionResult BookingPageForManager()
+        {
+            return View();
+        }
+
+
+
+    }
+}
